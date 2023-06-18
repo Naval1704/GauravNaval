016488f8b282c17bcfb3c1b7157b19586e99b168
NAVAL <108187176+Naval1704@users.noreply.github.com>
2023-06-18 12:34:37 +0530

    Update Gaurav_Naval.js

--- Gaurav_Naval.js
+++ Gaurav_Naval.js
@@ -9,45 +9,67 @@ window.addEventListener("load", function() {
 
 // Background body Cursor effect
 document.addEventListener('mousemove', function(event) {
-    var cursorShadow = document.querySelector('.cursor-shadow');
-    var radius = 400;
-    var color = '#211f31';
-    var shadowEffect = '0 0' + radius + 'px ' + color;
-    cursorShadow.style.top = event.pageY + 'px';
-    cursorShadow.style.left = event.pageX + 'px';
-    // cursorShadow.style.boxShadow = shadowEffect;
+  var cursorShadow = document.querySelector('.cursor-shadow');
+  var radius = 400;
+  var color = '#211f31';
+  var shadowEffect = '0 0 ' + radius + 'px ' + color;
+
+  var cursorX = event.clientX;
+  var cursorY = event.clientY;
+
+  // Calculate the adjusted top and left positions based on the cursor position and radius
+  var adjustedTop = cursorY - radius / 40;
+  var adjustedLeft = cursorX - radius / 40;
+
+  cursorShadow.style.top = adjustedTop + 'px';
+  cursorShadow.style.left = adjustedLeft + 'px';
+  // cursorShadow.style.boxShadow = shadowEffect;
 });
+
+
 
 // Toggle mobile menu
 document.addEventListener('DOMContentLoaded', function() {
-    var menuToggle = document.getElementById('menu-toggle');
-    var mobileMenu = document.getElementById('mobile-menu');
+  var menuToggle = document.getElementById('menu-toggle');
+  var mobileMenu = document.getElementById('mobile-menu');
 
-    menuToggle.addEventListener('change', function() {
-      if (menuToggle.checked) {
-        mobileMenu.style.right = '0';
-      } else {
-        mobileMenu.style.right = '-100%';
-      }
-    });
+  // Function to close the mobile menu
+  function closeMobileMenu() {
+    menuToggle.checked = false;
+    mobileMenu.style.right = '-100%';
+  }
+
+  // Event listener for menu toggle change
+  menuToggle.addEventListener('change', function() {
+    if (menuToggle.checked) {
+      mobileMenu.style.right = '0';
+    } else {
+      mobileMenu.style.right = '-100%';
+    }
   });
+
+  // Event listener for clicking on a menu item
+  mobileMenu.addEventListener('click', function() {
+    closeMobileMenu();
+  });
+
+  // Event listener for scrolling
+  window.addEventListener('scroll', function() {
+    closeMobileMenu();
+  });
+
+});
 
 // Front page Intro section
 
 // Variables to store the previous scroll position and scroll direction
-let prevScrollPos = window.pageYOffset;
+window.addEventListener('scroll', function() {
+  var header = document.getElementById('main-header');
+  var scrollPosition = window.scrollY || window.pageYOffset;
 
-function handleScroll() {
-  const currentScrollPos = window.pageYOffset;
-  const header = document.querySelector('header');
-
-  if (currentScrollPos > prevScrollPos) {
-    header.classList.remove('header-animation');
+  if (scrollPosition > 0) {
+    header.classList.add('scrolled');
   } else {
-    header.classList.add('header-animation');
+    header.classList.remove('scrolled');
   }
-
-  prevScrollPos = currentScrollPos;
-}
-
-window.addEventListener('scroll', handleScroll);
+});
